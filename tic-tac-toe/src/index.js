684cd6aaf47fad0c8ba3cfbb4dc592a4acdba5bb
Pedro Rocha <pedr0_r@hotmail.com>
2017-08-14 20:45:50 -0300

    Highlighting the winner squares. Fixed warnings.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={"square " + (props.winnerSquare ? "winner" : "")} onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -15,6 +15,7 @@ class Board extends React.Component {
     return (
     	<Square 
     		value={this.props.squares[i]}
+        winnerSquare={isWinnerSquare(this.props.winnerLines, i)}
     		onClick={() => this.props.onClick(i)}
     	/>
     )
@@ -77,28 +78,29 @@ class Game extends React.Component {
 	jumpTo(step) {
 		this.setState({
 			stepNumber: step,
-			xIsNext: (step % 2) == 0,
+			xIsNext: (step % 2) === 0,
 		});
 	}
 
   render() {
   	const history = this.state.history;
   	const current = history[this.state.stepNumber];
-  	const winner = calculateWinner(current.squares);
+  	let winnerMove = calculateWinner(current.squares);
 
   	const moves = history.map((step, move) => {
   		const desc = move ? ('Move #' + move) : 'Game Start';
   		return (
   			<li key={move}>
-  				<a href="#" onClick={() => this.jumpTo(move)}>{desc} { move != 0 ? '- Player: ' + (move % 2 == 0 ? 'O' : 'X') : ''}</a>
+  				<a href="#" onClick={() => this.jumpTo(move)}>{desc} { move !== 0 ? '- Player: ' + (move % 2 === 0 ? 'O' : 'X') : ''}</a>
   			</li>
   		);
   	});
 
   	let status;
-  	if (winner) {
-  		status = "Winner: " + winner;
+  	if (winnerMove) {
+  		status = "Winner: " + winnerMove.player;
   	} else {
+      winnerMove = [];
     	status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   	}
 
@@ -107,6 +109,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board 
           	squares={current.squares}
+            winnerLines={winnerMove.lines}
           	onClick={(i) => this.handleClick(i)}
           />
         </div>
@@ -117,6 +120,11 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function isWinnerSquare(winnerLines, i) {
+  if (winnerLines)
+    return (winnerLines.indexOf(i) > -1);
 }
 
 function calculateWinner(squares) {
@@ -134,7 +142,7 @@ function calculateWinner(squares) {
 	for (let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i];
 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
+			return {player: squares[a], lines: lines[i]};
 		}
 	}
 
@@ -142,12 +150,19 @@ function calculateWinner(squares) {
 		if (!squares[i]) 
 			return null;
 	}
-	return "Draw";
+	return {player: "Draw"};
+}
+
+function GameName(props) {
+  return <h1>{props.name}</h1>;
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <div>
+    <GameName name="Tic-tac-toe" />
+    <Game />
+  </div>,
   document.getElementById('root')
 );
